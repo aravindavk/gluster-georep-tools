45b67877e4c2705b31fc9c47abf066bdb6d8df36
Aravinda Vishwanathapura <mail@aravindavk.in>
2021-05-31 13:03:30 +0530

    glustercli library usage fix
    Also changed Master -> Primary and Slave to Secondary
    
    Signed-off-by: Aravinda Vishwanathapura <mail@aravindavk.in>

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-    gluster-georep-tools.setup.py
-    :copyright: (c) 2016 by Aravinda VK
-    :license: MIT, see LICENSE for more details.
-"""
-
 from setuptools import setup
 
 
 setup(
     name="gluster-georep-tools",
-    version="0.2",
+    version="0.3",
     packages=["gluster_georep_tools",
               "gluster_georep_tools.status",
               "gluster_georep_tools.setup"],
@@ -24,7 +18,7 @@ setup(
     },
     platforms="linux",
     zip_safe=False,
-    author="Aravinda VK",
+    author="Aravinda Vishwanathapura",
     author_email="mail@aravindavk.in",
     description="Gluster Geo-replication tools",
     license="MIT",
@@ -39,9 +33,6 @@ setup(
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 2 :: Only"
+        "Programming Language :: Python"
     ],
 )
